8d52987dd482fb346827548547cd6840ef8e7334
Marshall Clyburn <marshall@mardev.net>
2015-07-03 17:02:02 -0400

    Add commenting in File struct.

--- File.h
+++ File.h
@@ -15,6 +15,19 @@ struct File
     File(const std::string& name);
     ~File();
  
+    /*
+      'name'
+      The name of the file. This does not include any kind of path
+      to the file (neither relative nor full). Initialized to a blank
+      string.
+
+      'checksum'
+      The file's checksum. Corrcheck uses the SHA-256 algorithm
+      for hashes.
+
+      'next'
+      Enables linked-list structures. Initialized to 'NULL'.
+     */
     std::string name;
     unsigned char checksum[SHA256_DIGEST_LENGTH];
     File* next;
