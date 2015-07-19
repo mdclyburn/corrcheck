40bf5e30a89b8afab4e2e8c070f7316229bc3d65
Marshall Clyburn <marshall@mardev.net>
2015-07-19 00:28:51 -0400

    Implement checksum calculation.

--- File.cpp
+++ File.cpp
@@ -26,6 +26,40 @@ const std::string& File::get_path() const
 
 unsigned int File::calculate_checksum()
 {
+    // debug
+    assert(checksum == nullptr); // - please do not checksum twice
+    assert(path.length() > 0); // - a path should be specified
+
+    checksum = new unsigned char[SHA256_DIGEST_LENGTH];
+    
+    std::ifstream file;
+    file.open(path, std::ios::binary);
+    if(!file.is_open())
+	return FILE_OPEN_FAILED;
+    
+    SHA256_CTX sha;
+    SHA256_Init(&sha);
+    
+    unsigned char* buffer = new unsigned char[FILE_BUFFER_SIZE];
+    while(!file.eof())
+    {
+	// get up to FILE_BUFFER_SIZE byts of file data
+	unsigned int bytes_read = 0;
+	while(bytes_read < FILE_BUFFER_SIZE)
+	{
+	    file.read(reinterpret_cast<char*>(&buffer[bytes_read]), sizeof(unsigned char));
+	    if(file.eof()) break;
+	    else bytes_read++;
+	}
+	std::cout << "read " << bytes_read << " bytes" << std::endl;
+
+	SHA256_Update(&sha, buffer, bytes_read);
+    }
+
+    delete[] buffer;
+    file.close();
+    SHA256_Final(checksum, &sha);
+    
     return FILE_SUCCESS;
 }
 
--- File.h
+++ File.h
@@ -1,15 +1,19 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <cassert>
+#include <fstream>
+#include <iostream>
 #include <openssl/ssl.h>
 #include <string>
 
 // use buffer size of 10MB when handling files
-#define BUFFER_SIZE 10485760
+#define FILE_BUFFER_SIZE 10485760
 
 // error codes
 #define FILE_SUCCESS 0
 #define FILE_FAILURE 1
+#define FILE_OPEN_FAILED 2
 
 class File
 {
